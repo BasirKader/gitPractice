e763842365d33117a7659350541e3b5b4e87fdcb
BasirKader <basirkader33@gmail.com>
2022-02-16 19:26:54 -0500

    2nd commit

---
+++ src/Git/Testing.java
@@ -0,0 +1,10 @@
+package Git;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+        System.out.println("print hello");
+	}
+
+}
--- src/gitHomeWork/GitHW.java
+++ src/gitHomeWork/GitHW.java
@@ -5,7 +5,9 @@ public class GitHW {
 	public static void main(String[] args) {
 		
 		System.out.println("Basir");
-
+        System.out.println("Kader");
+       System.out.println("Nasir");
+       System.out.println("lil bro");
 	}
 
 }
